28a5b3736b03794f45fc0965fefd96a8af42ee87
Tyla Devon <tyladevon@gmail.com>
2020-01-28 21:22:58 -0700

    Create spec for Merchant Item's endpoint. Add route, serializer, controller action.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/merchants', to: 'merchants#index'
+      get '/merchants/:id/items', to: 'merchants#show'
     end 
   end 
 end
