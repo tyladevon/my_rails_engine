f1c49eb6ca0c7cbc0d5e15fecf8149768a3973b9
Tyla Devon <tyladevon@gmail.com>
2020-01-30 13:10:15 -0700

    refactor - merchants_items file. Create transaction factory

--- spec/requests/api/v1/relationships/merchants_items_spec.rb
+++ spec/requests/api/v1/relationships/merchants_items_spec.rb
@@ -11,7 +11,6 @@ describe "Merchants Items Endpoint" do
         expect(response).to be_successful
 
         merchants = JSON.parse(response.body, symbolize_names: true)
-        binding.pry
 
         expect(merchants[:data][0][:relationships][:items][:data].count).to eq(5)
     end 
