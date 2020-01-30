0fd316c5aeaf9d8e1b1f27076567233957586dc2
Tyla Devon <tyladevon@gmail.com>
2020-01-30 08:17:11 -0700

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/merchants', to: 'merchants#index'
-      get '/merchants/:id/items', to: 'merchants#show'
+      get '/merchants/find', to: 'merchants#find'
+      get '/merchants/:merch_id', to: 'merchants#show'
+      get '/merchants/:id/items', to: 'merchants#index'
+      get 'merchants/most_revenue', to: 'merchants#most_revenue'
     end 
   end 
 end
