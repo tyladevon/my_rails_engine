1505e64856f53583cda9ae94b07b151f4ce8c35b
Tyla Devon <tyladevon@gmail.com>
2020-01-30 13:06:14 -0700

    Refactor Items Record Endpoints in Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      # namespace :merchants do 
       get '/merchants/', to: 'merchants#index'
       get '/merchants/find', to: 'merchants#find'
       get '/merchants/find_all', to: 'merchants#find_all'
@@ -11,15 +10,12 @@ Rails.application.routes.draw do
       get '/merchants/:merch_id', to: 'merchants#show'
       get '/merchants/:merch_id/items', to: 'merchants#index'
       get '/merchants/most_revenue', to: 'merchants#most_revenue'
-      # end 
 
-    # namespace :items do 
       get '/items', to: 'items#index'
       get '/items/:item_id', to: 'items#show'
       get '/items/find', to: 'items#find'
       get '/items/find_all', to: 'items#find_all'
       get '/items/random', to: 'items#random'
-      # end
     end 
   end
 end 
