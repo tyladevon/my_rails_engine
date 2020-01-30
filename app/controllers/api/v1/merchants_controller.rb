47febfe8574574c1067d43f40cb294e047d332e0
Tyla Devon <tyladevon@gmail.com>
2020-01-30 09:28:24 -0700

    Add find_all and random methods in Merch Controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -17,9 +17,21 @@ class Api::V1::MerchantsController < ApplicationController
         render json: serialized_merchant
     end 
 
+    def find_all
+        merchants = Merchant.where(merch_params)
+        serialized_merchants = MerchantSerializer.new(merchants)
+        render json: serialized_merchants
+    end 
+
+    def random
+        merchant = Merchant.all.sample
+        serialized_merchant = MerchantSerializer.new(merchant)
+        render json: serialized_merchant
+    end 
+
     private 
 
     def merch_params
-        params.permit(:id, :name, :created_at, :updated_at)
+        params.permit(:merch_id, :name, :created_at, :updated_at)
     end 
 end 
