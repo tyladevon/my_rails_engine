02a86ce7da65c3df4162b39cd1d65f178910706b
Tyla Devon <tyladevon@gmail.com>
2020-01-30 21:44:39 -0700

    Refactor for Invoice Items Record Endpoints

--- spec/factories/invoice_item.rb
+++ spec/factories/invoice_item.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
-    factory :invoice_items do
+    factory :invoice_item do
+        quantity { Faker::Number.number }
+        unit_price { Faker::Commerce.price }
+
         association :item
         association :invoice
     end 
