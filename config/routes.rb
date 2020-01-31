6f5839b2bc7007023a08807b7813c36ad221b303
Tyla Devon <tyladevon@gmail.com>
2020-01-30 21:45:14 -0700

    Refactor routes for Invoice Items Record endpoints and adjust :id's

--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,31 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/merchants/', to: 'merchants#index'
+      get '/merchants', to: 'merchants#index'
       get '/merchants/find', to: 'merchants#find'
       get '/merchants/find_all', to: 'merchants#find_all'
       get '/merchants/random', to: 'merchants#random'
-      get '/merchants/:merch_id', to: 'merchants#show'
-      get '/merchants/:merch_id/items', to: 'merchants#index'
+      get '/merchants/:id', to: 'merchants#show'
+      get '/merchants/:id/items', to: 'merchants#index'
       get '/merchants/most_revenue', to: 'merchants#most_revenue'
 
       get '/items', to: 'items#index'
-      get '/items/:item_id', to: 'items#show'
       get '/items/find', to: 'items#find'
       get '/items/find_all', to: 'items#find_all'
       get '/items/random', to: 'items#random'
+      get '/items/:id', to: 'items#show'
 
       get '/invoices', to: 'invoices#index'
-      get '/invoices/:invoices_id', to: 'invoices#show'
       get '/invoices/find', to: 'invoices#find'
       get '/invoices/find_all', to: 'invoices#find_all'
       get '/invoices/random', to: 'invoices#random'
+      get '/invoices/:id', to: 'invoices#show'
+      
+      get '/invoice_items', to: 'invoice_items#index'
+      get '/invoice_items/find', to: 'invoice_items#find'
+      get '/invoice_items/find_all', to: 'invoice_items#find_all'
+      get '/invoice_items/random', to: 'invoice_items#random'
+      get '/invoice_items/:id', to: 'invoice_items#show'
     end 
   end
 end 
