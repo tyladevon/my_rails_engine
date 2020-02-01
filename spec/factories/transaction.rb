9465b6661d0fe1f01a905c13f8a03644138348f3
Tyla Devon <tyladevon@gmail.com>
2020-02-01 09:35:30 -0700

    Refactor transaction factory

--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
     factory :transaction do 
         invoice_id { Faker::Invoice}
         credit_card_number {Faker::Business.credit_card_number}
-        credit_card_expiration { Faker::Business.credit_card_expiry_date}
-        status {"success"}
+        credit_card_expiration_date { Faker::Business.credit_card_expiry_date}
+        result {"success"}
+        association :invoice
     end 
 end 
