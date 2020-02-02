eb55fb733b510f721d1eadb3c057b72dff9e7f22
Tyla Devon <tyladevon@gmail.com>
2020-02-02 16:33:02 -0700

    Create spec for Invoice-items Item relationship endpoint

--- spec/requests/api/v1/relationships/invoice_items/invoice_items_item_spec.rb
+++ spec/requests/api/v1/relationships/invoice_items/invoice_items_item_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
-describe "Invoices' Invoice_items Endpoint" do
+describe "Invoice_items Item Endpoint" do
     it "can get a list of Invoices' invoice_items" do 
         invoice = create(:invoice)
-        create_list(:invoice_item, 5,
-                invoice_id: invoice.id)
-
-        get "/api/v1/invoices/#{invoice.id}/invoice_items"
+        invoice_item = create(:invoice_item, invoice_id: invoice.id)
+       
+        get "/api/v1/invoice_items/#{invoice_item.id}/item"
 
         expect(response).to be_successful
 
-        invoice_items = JSON.parse(response.body, symbolize_names: true)
+        item = JSON.parse(response.body, symbolize_names: true)
 
-        expect(invoice_items[:data].count).to eq(5)
+        expect(item[:data][:id].to_i).to eq(invoice_item.item.id)
     end 
 end 
