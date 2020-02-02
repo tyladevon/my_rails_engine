ed17cebccea8eacb518ed8d6554d8c69c6504784
Tyla Devon <tyladevon@gmail.com>
2020-02-02 11:10:10 -0700

    Create controller for invoices' customers

---
+++ app/controllers/api/v1/invoices/customers_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Invoices::CustomersController < ApplicationController
+     def show
+        invoice = Invoice.find(params[:id])
+        serialized_customer = CustomerSerializer.new(invoice.customer)
+        render json: serialized_customer
+    end 
+end 
