8479ba1e5affa4bddede89fa40cf943350a99d2a
Tyla Devon <tyladevon@gmail.com>
2020-02-02 09:08:19 -0700

    Refactor factories for merchants invoice items and invoice

--- spec/factories/invoice.rb
+++ spec/factories/invoice.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
     factory :invoice do
-        association :customer 
-        association :merchant
-
         customer_id {customer.id}
         merchant_id {merchant.id}
         status {"shipped"}
+        association :customer 
+        association :merchant
     end 
 end 
