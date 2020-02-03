7920332cd3f60d3e1061f08b4daa5540a2033a71
Tyla Devon <tyladevon@gmail.com>
2020-02-03 05:24:40 -0700

    Work on Most Revenue Method of Merch

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,14 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   has_many :items
-  has_many :invoices
-  has_many :invoice_items, through: :invoices
-  has_many :transactions, through: :invoices
+  has_many :invoices, through: :items
+
+   def self.most_revenue(quantity)
+        joins(invoices: [:invoice_items, :transactions]).
+        select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price)as revenue").
+        group(:id).
+        where(transactions: {result: 'success'}).
+        order('revenue desc').
+        limit(quantity)
+    end 
 end
