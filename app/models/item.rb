f1e9e02686d353e8e2b40627e100aa4dba8fc09a
Tyla Devon <tyladevon@gmail.com>
2020-02-02 18:01:05 -0700

    Refactor Item to include relationship  with invoice-items and invoices

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,7 @@ class Item < ApplicationRecord
                         :unit_price,
                         :merchant_id
   belongs_to :merchant 
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
   before_save :change_unit_price
 end
